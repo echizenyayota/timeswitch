5edc55d35b5025c0dd015efb09b92b9b135a7610
echizenyayota <virtualmisso30@gmail.com>
2017-01-24 16:57:05 +0900

    index.phpからフッター部分をfooter.phpに分離

---
+++ wp-content/themes/TimeSwitch/footer.php
@@ -0,0 +1,11 @@
+    <div class="box5">
+        <div class="box5-inner">
+            <div class="copyright">
+                <?php bloginfo( 'description' ); ?>
+                <p>Copyright&copy; 2017<?php if (date("Y")!=2012) echo date("-Y"); ?> All rights reserved, TimeSwitch</p>
+            </div>
+        </div>
+    </div>
+    <?php wp_footer(); ?>
+</body>
+</html>
