45f6016374db206502432aa208653b82aa543c4b
echizenyayota <virtualmisso30@gmail.com>
2017-01-24 20:19:18 +0900

    footer.phpの2017の文字列を消す

--- wp-content/themes/TimeSwitch/footer.php
+++ wp-content/themes/TimeSwitch/footer.php
@@ -2,7 +2,7 @@
         <div class="box5-inner">
             <div class="copyright">
                 <?php bloginfo( 'description' ); ?>
-                <p>Copyright&copy; 2017<?php if (date("Y")!=2012) echo date("-Y"); ?> All rights reserved, TimeSwitch</p>
+                <p>Copyright&copy; 2017<?php if (date("Y")!=2017) echo date("-Y"); ?> All rights reserved, TimeSwitch</p>
             </div>
         </div>
     </div>
